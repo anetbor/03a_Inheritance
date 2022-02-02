0de66e45f59bacf3cc432c89b44ea7612fb1e779
Peter <schmidt.multimedia@gmail.com>
2022-02-02 10:29:37 +0100

    Tomcat fertig

---
+++ com/cc/java/Tomcat.java
@@ -0,0 +1,33 @@
+package com.cc.java;
+
+public class Tomcat {
+    
+    private String name;
+    private String furColor;
+    private int age;
+
+    public Tomcat(String name, String furColor, int age) {
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+    }
+
+       /** Getter */
+    public String getStringAttributes(String flag) {
+        switch (flag) {
+            case "#name":
+                return this.name; 
+            case "#furColor":
+                return this.furColor; 
+            default:
+                return "ERROR: ???";
+        }
+    }
+
+    public String getAge() {
+        return String.valueOf(this.age);
+    }
+
+
+
+}
